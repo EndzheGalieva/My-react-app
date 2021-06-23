9f5d8edc8822fc40a8640a56a6339c7c4814e5d4
Endzhe Galieva <endzhe91@gmail.com>
2021-06-23 19:24:20 +0300

    use the children prop

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-import { useLocation  } from 'react-router-dom';
-import { parse } from 'query-string';
-import { CounterButton } from "../CounterButton";
-import { CongratulationsMessage } from "../CongratulationsMessage";
+import React, {useState} from 'react';
+import {useLocation} from 'react-router-dom';
+import {parse} from 'query-string';
+import {CounterButton} from "../CounterButton";
+import {CongratulationsMessage} from "../CongratulationsMessage";
+import {DisplayIf} from '../DisplayIf';
 
 export const CounterButtonPage = () => {
   const location = useLocation();
-  const startingValue = parse(location.search).startingValue;
+  const startingValue = parse(location.search).startingValue || 0;
   const [numberOfClicks, setNumberOfClicks] = useState(Number(startingValue));
   const [hideMessage, setHideMessage] = useState(false);
 
   const increment = () => setNumberOfClicks(numberOfClicks + 1);
 
   return (
-      <>
+    <>
       <h1>The Counter Button Page</h1>
-      {hideMessage
-       ? null
-       : <CongratulationsMessage
-           numberOfClicks={numberOfClicks}
-           threshold={10}
-           onHide={() => setHideMessage(true)} />}
-        <CounterButton onIncrement={increment} numberOfClicks={numberOfClicks} />
+      <DisplayIf condition={!hideMessage && numberOfClicks >= 10}>
+        <CongratulationsMessage
+          threshold={10}
+          onHide={() => setHideMessage(true)}/>
+      </DisplayIf>
+      <CounterButton onIncrement={increment} numberOfClicks={numberOfClicks}/>
     </>
   );
 }
